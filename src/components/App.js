365a2b9a89e3f75bdcb8033b1dd0608a83e48c18
kayambi <you@example.com>
2020-08-20 23:50:04 -0700

    first Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,27 @@
 import React, {Component} from 'react';
+// import Info from './Info';
+import Header from './Header';
+// import './index.css';
+import Game from './Game';
 
+ class App extends Component {
+       render() {
+          return (
+            <React.Fragment>
+            <div className="info">
+                 {/* <h1>Hello I wil do it better {this.props.name}</h1>
+                 <p>Another Name {this.props.name}</p> */}
+                 {/* Calling Game Components */}
+                 <Game/>  
+                 {/* Calling Info Component */}
+                 {/* <Info/> */}
+                 {/* Calling Header Component */}
+                 <Header/>
+.            </div>
+            </React.Fragment>
+            );
+          }
 
-class Game extends Component {
-
-  render(){
-    return ();
-  }
-}
-
-export default Game;
+        }
+  
+ export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 
-const Header = (props) => {
-  return();
+class Header extends Component {
+// const Header = (props) => {
+  runFunc =() =>{
+    this.setState=({
+      open:true
+    })
+  }
+  render(){
+  return( 
+    // Fragment eleminate the need to  wrapp every component into div
+    <React.Fragment> 
+        <div className="main-header">
+            <h1>TicTocToe 
+              <button class="button" onClick ={this.runFunc}>Start</button> 
+            </h1>
+           
+        </div>
+     </React.Fragment>
+  );
+  }
 }
-
 export default Header;
